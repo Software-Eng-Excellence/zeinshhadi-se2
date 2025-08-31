e48c828d7ea70eb2f7b85e3e0101b0b6fe2bb581
zeinshhadi <zeinshhadi@gmail.com>
2025-08-31 22:41:08 +0000

    Initializing project

--- feature.ts
+++
@@ -1,1 +0,0 @@
-let feature: string = "new feature branch";
---
+++ tests/example.test.ts
@@ -0,0 +1,5 @@
+describe('Example Test Suite', () => {
+    it('Example Test Case', () => {
+        expect(1 + 1).toBe(2);
+    });
+});
